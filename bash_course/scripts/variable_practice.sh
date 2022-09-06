60b7973e8314e210728c5d37cefc31c81c793def
AShakerQA <shaker.abdul@hotmail.com>
2022-09-06 11:15:54 +0100

    added professional comments

--- bash_course/scripts/variable_practice.sh
+++ bash_course/scripts/variable_practice.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# Author: Shak
+# Date Created: 06/09/2022
+# Last Modified: 06/09/2022
+
+# Description
+# Prints Hello then the student variable
+
+# Usage
+# variable_practice
+
+
+
 student="Shak"
 
 echo "Hello ${student}"
